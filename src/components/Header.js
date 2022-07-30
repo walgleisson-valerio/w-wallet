84d6fce74d750fdd1ef92fe3293deb552de8ec85
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-07-29 21:14:15 -0300

    Resolve requisto 02

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { userEmail } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <h3>TrybeWallet</h3>
+        <h3 data-testid="email-field">{ userEmail }</h3>
+        <span>Gasto totais:</span>
+        <span data-testid="total-field">
+          0
+        </span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
