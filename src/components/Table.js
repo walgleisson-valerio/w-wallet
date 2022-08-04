53b68221e9f713feb95ff339f1bd243f7b481caf
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-08-03 21:30:07 -0300

    Resolve requisto 07

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
+  getTotal = (valueString, currencyString) => {
+    const value = valueString ? parseFloat(valueString) : 0;
+    const currency = parseFloat(currencyString);
+    const result = value * currency;
+
+    return result.toFixed(2);
+  }
+
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
       <table>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map(({
+            id,
+            description,
+            currency,
+            tag,
+            method,
+            value,
+            exchangeRates,
+          }) => (
+            <tr key={ id }>
+              <td>{ description }</td>
+              <td>{ tag }</td>
+              <td>{ method }</td>
+              <td>{ parseFloat(value).toFixed(2) }</td>
+              <td>
+                { exchangeRates[currency].name }
+              </td>
+              <td>{ parseFloat(exchangeRates[currency].ask).toFixed(2) }</td>
+              <td>{ this.getTotal(value, exchangeRates[currency].ask) }</td>
+              <td>Real</td>
+              <td>Editar/Excluir</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
